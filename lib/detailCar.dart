e4bf4096a22f8219236f3caa97bf3bc8c9ea09ad
ChuTich47 <phamtruongd022@gmail.com>
2024-03-07 09:56:54 +0700

    change Detail2

--- lib/detailCar.dart
+++ lib/detailCar.dart
@@ -7,13 +7,11 @@ class DetailCar extends StatefulWidget {
   State<DetailCar> createState() => _DetailCarState();
 }
 
-class _DetailCarState extends State<DetailCar> with SingleTickerProviderStateMixin {
-
-
+class _DetailCarState extends State<DetailCar>
+    with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
@@ -25,7 +23,30 @@ class _DetailCarState extends State<DetailCar> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        child: Column(
+          children: [
+            Card(
+                margin: EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                )),
+            Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("Kia Moring",
+                          style: TextStyle(fontSize: 14, color: Colors.green)),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
