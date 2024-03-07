b538b544894436382bac8d0bf5f1262e71b039f0
ChuTich47 <phamtruongd022@gmail.com>
2024-03-07 09:42:05 +0700

    change Detail

---
+++ lib/detailCar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DetailCar extends StatefulWidget {
+  const DetailCar({super.key});
+
+  @override
+  State<DetailCar> createState() => _DetailCarState();
+}
+
+class _DetailCarState extends State<DetailCar> with SingleTickerProviderStateMixin {
+
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  void dispose() {
+    // _controller.dispose();
+    // super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
