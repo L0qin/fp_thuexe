708b9170703035df1afed2c11013a5bc91fd7f1f
ChuTich47 <phamtruongd022@gmail.com>
2024-03-19 17:15:24 +0700

    change detai

--- lib/pages/car_detail_page.dart
+++ lib/pages/car_detail_page.dart
@@ -3,7 +3,10 @@ import 'dart:async';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fp_thuexe/services/AuthService.dart';
 import 'package:fp_thuexe/services/ImageService.dart';
+import 'package:fp_thuexe/services/UserService.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 import '../models/Vehicle.dart';
@@ -63,6 +66,18 @@ class _DetailCarState extends State<DetailCar> {
     }
   }
 
+  Future<int?> getUserIdFromSharedPrefs() async {
+    try {
+      final userId = await AuthService.getUserId();
+      return userId;
+    } catch (error) {
+      print('Error retrieving user ID: $error');
+      return null; // Handle potential errors
+    }
+  }
+
+
+
   Future<void> _getImageURLs() async {
     try {
       List<String> imageURLs =
@@ -90,15 +105,19 @@ class _DetailCarState extends State<DetailCar> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               _buildCarouselSlider(),
-              _buildTextWithIconRow(
-                  _vehicle != null ? _vehicle!.carName : "...",
-                  _vehicle != null
-                      ? "${_vehicle!.rentalPrice}VNĐ/Ngày"
-                      : "500VNĐ/Ngày"),
-              _buildDescription(
-                  _vehicle != null ? _vehicle!.description : "..."),
-              _buildLocationSelection(
-                  _vehicle != null ? _vehicle!.address : "Katowice Airport"),
+              // _buildTextWithIconRow(
+              //     _vehicle != null ? _vehicle!.carName : "...",
+              //     _vehicle != null
+              //         ? "${_vehicle!.rentalPrice}VNĐ/Ngày"
+              //         : "500VNĐ/Ngày"),
+
+              // _buildDescription(
+              //     _vehicle != null ? _vehicle!.description : "..."),
+              _buildOwnerInfo(_vehicle!),
+              _buildCarInfo(_vehicle!),
+              // _buildLocationSelection(
+              //     _vehicle != null ? _vehicle!.address : "Katowice Airport"),
+              _buildOwner(),
               _buildElevatedButton(),
             ],
           ),
@@ -185,6 +204,7 @@ class _DetailCarState extends State<DetailCar> {
     }
   }
 
+// xe giá
   Widget _buildTextWithIconRow(String title, String price) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 12),
@@ -216,6 +236,189 @@ class _DetailCarState extends State<DetailCar> {
       ),
     );
   }
+  Widget _buildOwnerInfo(Vehicle ownerName) {
+    if (ownerName.ownerName != null) {
+      return Row(
+        children: [
+          SizedBox(width: 15,),
+          Icon(Icons.account_circle, size: 35.0, color: Colors.orange),
+          SizedBox(width: 4.0),
+          Text(
+            ownerName.carName,
+            style: TextStyle(fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.w800),
+          ),
+
+      ]);
+    } else {
+      return SizedBox(); // No owner information available
+    }
+  }
+
+  Widget _buildCarInfo(Vehicle car) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      // decoration: BoxDecoration(
+      //   borderRadius: BorderRadius.circular(12.0),
+      //   color: Colors.grey[200], // Light background
+      // ),
+      child: Column(
+        children: [
+          Divider(height: 4.0, thickness: 1.0, color: Colors.grey[400]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.car_rental_rounded, size: 35.0, color: Colors.red),
+
+              Text(
+                car.carName,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(width:280,),
+            ],
+          ),
+          Divider(height: 4.0, thickness: 1.0, color: Colors.grey[400]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+              Icon(Icons.pending_actions_rounded, size: 35.0, color: Colors.red),
+              Text(
+                '${car.manufacturer}',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(width: 100.0),
+              Icon(Icons.model_training, size: 35.0, color: Colors.red),
+
+              Text(
+                ' ${car.model}',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(width: 20,),
+            ],
+          ),
+          Divider(height: 4.0, thickness: 1.0, color: Colors.grey[400]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.money, size: 30.0, color: Colors.teal),
+                  Text(
+                    '${car.rentalPrice.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20.0,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+
+                  Icon(Icons.person, size: 35.0, color: Colors.teal),
+                  Text(
+                    ' ${car.seating}',
+                    style: TextStyle(fontSize: 20.0, color: Colors.black,fontWeight: FontWeight.w800),
+                  ),
+                  SizedBox(width: 100,),
+                ],
+              ),
+            ],
+          ),
+          Divider(height: 4.0, thickness: 1.0, color: Colors.grey[400]),
+
+          Row(
+            children: [
+              Icon(Icons.location_on, size: 35.0, color: Colors.red),
+              SizedBox(width: 4.0),
+
+              Text(
+                car.address != null ? car.address : "Address not available",
+                style: TextStyle(fontSize: 20.0, color: Colors.black,fontWeight: FontWeight.w800),
+              ),
+            ],
+          ),
+          Divider(height: 4.0, thickness: 1.0, color: Colors.grey[400]),
+          // Optionally add rows for additional details (check if available in Vehicle class)
+        ],
+      ),
+    );
+  }
+  Widget _buildOwner() {
+    return Container(
+      margin: EdgeInsets.only(left: 12,right: 12,bottom: 12),
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.grey[200], // Light background
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // SizedBox(width: 80),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: () {
+                  // Add share functionality
+                },
+                icon: Icon(
+                  Icons.share,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+              ),
+              SizedBox(width: 4.0),
+              IconButton(
+                onPressed: () {
+                  // Add message functionality
+                },
+                icon: Icon(
+                  Icons.message,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+              ),
+              SizedBox(width: 4.0),
+              TextButton(
+                onPressed: () {
+                  // Add phone call functionality
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 4.0),
+                    Text(
+                      'liên hệ  ',
+                      style: TextStyle(fontSize: 16.0, color: Colors.teal, fontWeight: FontWeight.w800),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
   Widget _buildDescription(String description) {
     return Padding(
@@ -228,10 +431,9 @@ class _DetailCarState extends State<DetailCar> {
             child: Text(
               "Mô tả",
               style: TextStyle(
-                fontSize: 21,
-                color: Colors.teal,
-                fontWeight: FontWeight.bold
-              ),
+                  fontSize: 21,
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold),
             ),
           ),
           SizedBox(height: 1),
@@ -296,4 +498,3 @@ class _DetailCarState extends State<DetailCar> {
     );
   }
 }
-
